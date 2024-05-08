9410373d6853f44104870f74bc7a7ba6003af984
Toru Dick <banvietthenaocungdc@gmail.com>
2024-05-08 11:45:48 +0700

    Constructed the PostgresSQL entity for the song.

---
+++ src/songs/song.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('song')
+export class Song {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('varchar', { array: true })
+  artists: string[];
+
+  @Column({ type: 'date' })
+  releasedDate: Date;
+
+  @Column({ type: 'time' })
+  duration: Date;
+
+  @Column({ type: 'text' })
+  lyrics: string;
+}
